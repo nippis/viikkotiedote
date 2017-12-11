1818476f01954395007e67e74e5b40df875ba350
Petteri Niemi <petteri.niemi@student.tut.fi>
2017-12-11 22:46:08 +0200

    Aloitettu pääikkunan toteutus

--- Viikkotiedote/mainwindow.cpp
+++ Viikkotiedote/mainwindow.cpp
@@ -3,9 +3,40 @@
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    QWidget *widget = new QWidget;
+    setCentralWidget(widget);
+
+    setWindowTitle("Viikkotiedotegeneraattori");
+    resize(600, 400);
+
+    QVBoxLayout *mainLayout = new QVBoxLayout;
+    widget->setLayout(mainLayout);
+
+    // Pääleiskan täytteet
+    QLabel *mainLabel = new QLabel();
+    mainLabel->setText("Viikkotiedote:");
+    mainLayout->addWidget(mainLabel);
+
+    tiedoteLayout_ = new QVBoxLayout;
+    mainLayout->addLayout(tiedoteLayout_);
+
+    QHBoxLayout *buttonLayout = new QHBoxLayout;
+
+    buttonAddTiedote_ = new QPushButton();
+    buttonAddTiedote_->setText("Lisää tiedote");
+    buttonLayout->addWidget(buttonAddTiedote_);
+    mainLayout->addLayout(buttonLayout);
+
+    createMenus();
 }
 
 MainWindow::~MainWindow()
 {
+
+}
+
+void MainWindow::createMenus()
+{
+    fileMenu_ = menuBar()->addMenu("File");
 
 }
--- Viikkotiedote/mainwindow.hh
+++ Viikkotiedote/mainwindow.hh
@@ -2,6 +2,12 @@
 #define MAINWINDOW_HH
 
 #include <QMainWindow>
+#include <QMenu>
+#include <QMenuBar>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QLabel>
+#include <QPushButton>
 
 class MainWindow : public QMainWindow
 {
@@ -10,6 +16,16 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = 0);
     ~MainWindow();
+
+
+private:
+    QMenu *fileMenu_;
+
+    QVBoxLayout *tiedoteLayout_;
+
+    QPushButton *buttonAddTiedote_;
+
+    void createMenus();
 };
 
 #endif // MAINWINDOW_HH
