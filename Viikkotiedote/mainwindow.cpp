05068844cc385f156148ac1988946d00e56a2704
Petteri Niemi <petteri.niemi@student.tut.fi>
2017-12-11 20:49:01 +0200

    Luotu projekti pääikkunalla ja muokattu gitignore tomivaksi

---
+++ Viikkotiedote/mainwindow.cpp
@@ -0,0 +1,11 @@
+#include "mainwindow.hh"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+{
+}
+
+MainWindow::~MainWindow()
+{
+
+}
---
+++ Viikkotiedote/mainwindow.hh
@@ -0,0 +1,15 @@
+#ifndef MAINWINDOW_HH
+#define MAINWINDOW_HH
+
+#include <QMainWindow>
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+};
+
+#endif // MAINWINDOW_HH
